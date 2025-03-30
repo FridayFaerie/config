d5e085f6a26e1cc19a24a51b5f647f2b6ac27ae6
Friday <faeriethe@gmail.com>
2025-03-30 00:57:44 +0000

    attempt at adding home-manager to this flake too

--- flake.nix
+++ flake.nix
@@ -4,27 +4,50 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    #home-manager = {
-    #  url = "github:nix-community/home-manager";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
-
     auto-cpufreq = {
       url = "github:AdnanHodzic/auto-cpufreq";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+
+# Home-manager things
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixCats = {
+      url = "git+file:///home/friday/nix-inputs/nixCats/";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    quickshell = {
+      url = "git+file:///home/friday/nix-inputs/quickshell/";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+
   };
 
   outputs = { self, nixpkgs, auto-cpufreq, ... }@inputs: 
   let
-    system = "x86_64-linuc";
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        allowUnfree = true;
-      };
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    custom-packages = {
+      snitch = import ./packages/snitch.nix { inherit nixpkgs; };
     };
+    # pkgs = import nixpkgs {
+    #   inherit system;
+    #   config = {
+    #     # allowUnfree = true;
+    #   };
+    # };
   in
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
@@ -34,6 +57,24 @@
         #inputs.home-manager.nixosModules.default
 	auto-cpufreq.nixosModules.default
       ];
+    };
+
+    homeConfigurations."friday" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      # Specify your home configuration modules here, for example,
+      # the path to your home.nix.
+      modules = [ 
+        ../home-manager/home.nix 
+        ../home-manager/wayland.nix
+      ];
+
+      # Optionally use extraSpecialArgs
+      # to pass through arguments to home.nix
+      extraSpecialArgs = { 
+        custompkgs =  inputs;
+        inherit custom-packages;
+      };
     };
   };
 }
