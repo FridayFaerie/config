712c002137b54c3e25f30dc5b734ea6961c2e119
Friday <faeriethe@gmail.com>
2025-04-02 20:33:46 +0100

    rebuilding home

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -35,6 +35,8 @@ in
     pkgs.kdePackages.qt5compat
     pkgs.kdePackages.kirigami.unwrapped
 
+pkgs.lolcat
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
