d2a3fd7c875ecc158b2a6d1dc22ec2ede1ac0f73
Friday <faeriethe@gmail.com>
2025-04-13 01:34:18 +0100

    rebuilding system

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,6 +4,15 @@
 
 { config, pkgs, inputs, ... }:
 
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -243,13 +252,6 @@
     wf-recorder
     busybox
 
-
-    #usefuls
-    typst
-    clang
-    clang-tools
-
-
     # Smaller terminal things
     # neo
     # lavat
@@ -260,6 +262,10 @@
     nitch
     leaf
 
+#usefuls
+    typst
+    clang
+    clang-tools
 
     # Hyprland things
     # waybar
@@ -279,6 +285,8 @@
     networkmanagerapplet
     yazi
     kitty
+
+    nvidia-offload
 
 
     # Bigger programs
