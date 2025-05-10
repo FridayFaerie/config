e741e1a820352f109eb30c548d229c99ef401fd5
Friday <faeriethe@gmail.com>
2025-05-10 01:52:09 +0100

    rebuilding home

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -139,32 +139,14 @@ in {
 
   textfox = {
     enable = true;
-    profile = "firefox profile name here";
+    profile = "Default";
     config = {
-      background = {
-        color = "#123456";
-      };
-      border = {
-        color = "#654321";
-        width = "4px";
-        transition = "1.0s ease";
-        radius = "3px";
-      };
-      displayHorizontalTabs = true;
-      displayWindowControls = true;
-      displayNavButtons = true;
-      displayUrlbarIcons = true;
-      displaySidebarTools = false;
-      displayTitles = false;
-      newtabLogo = "   __            __  ____          \A   / /____  _  __/ /_/ __/___  _  __\A  / __/ _ \\| |/_/ __/ /_/ __ \\| |/_/\A / /_/  __/>  </ /_/ __/ /_/ />  <  \A \\__/\\___/_/|_|\\__/_/  \\____/_/|_|  ";
-      font = {
-        family = "Fira Code";
-        size = "15px";
-        accent = "#654321";
-      };
-      sidebery = {
-        margin = "1.0rem";
-      };
+      extraConfig = ''
+        #tabbrowser-tabbox::before {
+          margin: -1.3rem 0rem;
+        }
+      '';
+      font.family = "Courier20, Courier13, Courier, monospace";
     };
   };
 }
