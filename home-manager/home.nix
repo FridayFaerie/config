773cd7a3a8aac01ecc8014c5840ba8e25d752eb8
Friday <faeriethe@gmail.com>
2025-05-10 01:47:52 +0100

    rebuilding home

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -136,4 +136,35 @@ in {
       "5"
     ];
   };
+
+  textfox = {
+    enable = true;
+    profile = "firefox profile name here";
+    config = {
+      background = {
+        color = "#123456";
+      };
+      border = {
+        color = "#654321";
+        width = "4px";
+        transition = "1.0s ease";
+        radius = "3px";
+      };
+      displayHorizontalTabs = true;
+      displayWindowControls = true;
+      displayNavButtons = true;
+      displayUrlbarIcons = true;
+      displaySidebarTools = false;
+      displayTitles = false;
+      newtabLogo = "   __            __  ____          \A   / /____  _  __/ /_/ __/___  _  __\A  / __/ _ \\| |/_/ __/ /_/ __ \\| |/_/\A / /_/  __/>  </ /_/ __/ /_/ />  <  \A \\__/\\___/_/|_|\\__/_/  \\____/_/|_|  ";
+      font = {
+        family = "Fira Code";
+        size = "15px";
+        accent = "#654321";
+      };
+      sidebery = {
+        margin = "1.0rem";
+      };
+    };
+  };
 }
