fa128000eb333ddd6e5529f11c335bd4e20bde14
Friday <faeriethe@gmail.com>
2025-05-13 01:23:05 +0100

    rebuilding system

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -136,12 +136,13 @@ in {
 
   nix.settings.auto-optimise-store = true;
 
-  nix.settings.trusted-users = ["root" "friday"];
+  # TODO: remove this line
+  # nix.settings.trusted-users = ["root" "friday"];
+  # nix.settings.trusted-public-keys = [
+  #   "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+  #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  # ];
 
-  nix.settings.trusted-public-keys = [
-    "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  ];
   nix.settings = {
     extra-substituters = [
       "https://hyprland.cachix.org"
