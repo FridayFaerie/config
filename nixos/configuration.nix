d237df2e078743955b13cec8b06df1ffc82dfdb2
Friday <faeriethe@gmail.com>
2025-05-13 02:02:33 +0100

    rebuilding system

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -44,8 +44,8 @@ in {
 
     # Smaller terminal things
     # neo
-    # lavat
-    asciiquarium
+    lavat
+    # asciiquarium
     # globe-cli
     # clolcat
     # fortune
@@ -148,11 +148,11 @@ in {
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
     ];
-    extra-trusted-substituters = [
+    trusted-substituters = [
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
     ];
-    extra-trusted-public-keys = [
+    trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
