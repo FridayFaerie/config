2de0900ff1cbfa11e5f4ad368222f2c3adc4bc0d
Friday <faeriethe@gmail.com>
2025-07-17 13:27:55 +0000

    rebuilt system     at 13:27:55 17/07/2025 UTC

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -145,7 +145,7 @@ in {
   # system.autoUpgrade.enable = true;
   # system.autoUpgrade.dates = "weekly";
 
-  # nix.package = pkgs.nixVersions.git;
+  nix.package = pkgs.nixVersions.nix_2_30;
 
   nix.settings = {
     auto-optimise-store = true;
