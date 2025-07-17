5d25fd208309a9cd6586f455ccdc662329266b8e
Friday <faeriethe@gmail.com>
2025-07-17 13:44:01 +0000

    rebuilt system     at 13:44:01 17/07/2025 UTC

--- iso/configuration.nix
+++ iso/configuration.nix
@@ -19,6 +19,12 @@
   nixpkgs = {
     hostPlatform = lib.mkDefault "x86_64-linux";
     config.allowUnfree = true;
+
+    overlays = [
+      (self: super: {
+        jdk8 = self.temurin-bin-8;
+      })
+    ];
   };
 
   nix.settings.experimental-features = [
